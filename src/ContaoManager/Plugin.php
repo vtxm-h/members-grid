b06cf62c20d47e1e4645206034fd790ee9339f41
Ben <83883581+vtxm-h@users.noreply.github.com>
2026-01-24 19:01:33 +0100

    Update Plugin.php

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,19 +1,18 @@
 <?php
-declare(strict_types=1);
 
 namespace Vendor\MembersGridBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
-use Contao\ManagerPlugin\Bundle\Config\BundleConfigInterface;
+use Vendor\MembersGridBundle\MembersGridBundle;
 
 class Plugin implements BundlePluginInterface
 {
     public function getBundles(array $bundles): array
     {
         return [
-            BundleConfig::create(\Vendor\MembersGridBundle\MembersGridBundle::class)
+            BundleConfig::create(MembersGridBundle::class)
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
