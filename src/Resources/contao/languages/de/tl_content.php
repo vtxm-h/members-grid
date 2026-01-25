93d1a81cea66bdc60479f682130de303c74eab6e
Ben <83883581+vtxm-h@users.noreply.github.com>
2026-01-25 07:53:58 +0100

    Create tl_content.php

---
+++ src/Resources/contao/languages/de/tl_content.php
@@ -0,0 +1,18 @@
+<?php
+
+$GLOBALS['TL_LANG']['CTE']['components'] = 'Komponenten';
+$GLOBALS['TL_LANG']['CTE']['members_grid'] = ['Team-Grid', '4-Personen-Cutout-Grid (links/oben/rechts/unten).'];
+
+$GLOBALS['TL_LANG']['tl_content']['members_legend'] = 'Team';
+
+$GLOBALS['TL_LANG']['tl_content']['member_left_img']   = ['Bild links', 'Cutout-Bild (links).'];
+$GLOBALS['TL_LANG']['tl_content']['member_left_name']  = ['Name links', 'Beschriftung unter dem linken Cutout.'];
+
+$GLOBALS['TL_LANG']['tl_content']['member_top_img']    = ['Bild oben', 'Cutout-Bild (oben mittig).'];
+$GLOBALS['TL_LANG']['tl_content']['member_top_name']   = ['Name oben', 'Beschriftung unter dem oberen Cutout.'];
+
+$GLOBALS['TL_LANG']['tl_content']['member_right_img']  = ['Bild rechts', 'Cutout-Bild (rechts).'];
+$GLOBALS['TL_LANG']['tl_content']['member_right_name'] = ['Name rechts', 'Beschriftung unter dem rechten Cutout.'];
+
+$GLOBALS['TL_LANG']['tl_content']['member_bottom_img'] = ['Bild unten', 'Cutout-Bild (unten mittig).'];
+$GLOBALS['TL_LANG']['tl_content']['member_bottom_name']= ['Name unten', 'Beschriftung unter dem unteren Cutout.'];
